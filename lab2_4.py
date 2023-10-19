8e59076835d07fde2a5789fa6e150405056dcbfb
mihailgenshin <146358174+mihailgenshin@users.noreply.github.com>
2023-10-19 16:48:54 +0000

    gergderg

--- lab2_4.py
+++ lab2_4.py
@@ -1,4 +1,4 @@
-a=int(input('Введите значение а: ') )
+a=int(input('Введите сколько палок закину в попу вадима: ') )
 b=int(input('Введите значение b: ') )
 N=int(input('Введите значение N: ') )
 for i in range(N):
@@ -6,3 +6,8 @@ for i in range(N):
     a=b
     b=c
     print(c)
+
+
+
+
+
