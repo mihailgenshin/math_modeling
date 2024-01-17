f48cf5050f41eacb27dc799a4c0d351758afe2bd
mihailgenshin <146358174+mihailgenshin@users.noreply.github.com>
2024-01-17 19:13:56 +0000

    dftijk

---
+++ lec_7_parametrikplote1.py
@@ -0,0 +1,33 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+from  matplotlib.animation import FuncAnimation
+
+
+def circle_move(R, angle_vel, time):
+    alpha = angle_vel * np.pi / 180 * time
+    x = R * np.cos(alpha)
+    y = R * np.sin(alpha)
+    return x, y
+
+
+def animate(i):
+    ball.set_data(circle_move(R=2, angle_vel=1, time=i))
+     
+
+if __name__ == '__main__':
+
+    fig, ax = plt.subplots()
+    ball, = plt.plot([], [], 'o', color='r', label='Ball')
+ 
+    edge = 3
+    plt.axis('equal')
+    ax.set_xlim(-edge, edge)
+    ax.set_ylim(-edge, edge)
+ 
+    ani = FuncAnimation(fig,
+                        animate,
+                        frames=720,
+                        interval=30)
+ 
+    ani.save('animation_2.gif') 
+
